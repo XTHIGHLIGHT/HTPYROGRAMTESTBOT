31df400c2ab37c8f0ebec1e25b96ce8a078b7b26
HIGHLIGHT BOTS <97348810+XTHIGHLIGHT@users.noreply.github.com>
2022-01-24 20:56:51 +0530

    Create highlight.py

--- highlight.py
+++ highlight.py
@@ -1,1 +1,15 @@
+from pyrogram import client, filters
 
+Highlight=client(
+    "Pyrogram bot",
+    bot_token="5228659986:AAH93K7xVMwThS_u1FUaq6pN1govoq6wZQQ",
+    api_id="17827478",
+    api_hash="22a35b5545535be40a1efae412b1b6b8"
+)
+
+@Highlight.on_message(filters.command("start")) 
+async def start_message(bot, message):
+    await message.reply_text("hi")
+
+
+Highlight.run()
