7020b7f72132ceaa7e228f18cfc448e5f4dd38f6
HIGHLIGHT BOTS <97348810+XTHIGHLIGHT@users.noreply.github.com>
2022-02-14 06:38:17 +0530

    Update highlight.py

--- highlight.py
+++ highlight.py
@@ -1,122 +1,52 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-import random
-import os
-from sample_config import Config
 
-Devourd=Client(
+Highlight=Client(
     "Pyrogram bot",
     bot_token="5071694006:AAGIeZuN6F_31aEW-PE-lNr4bRjq26rA4HM",
     api_id="17827478",
     api_hash="22a35b5545535be40a1efae412b1b6b8"
 )
 
-
-
-
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg"
-]
-
-
-
-
-
-
-
-@Devourd.on_message(filters.command("start")) 
-async def start_message(bot, message): 
+@Highlight.on_message(filters.command("start")) 
+async def start_message(bot, message):
     await message.reply_photo(
-        photo=random.choice(ALL_PIC),
-        caption=f"""CLICK AND JOIN {message.from_user.mention}""",
+        photo="https://telegra.ph/file/6f014e3d6c8b90cb6d49e.jpg",
         reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton("click this to join", url="t.me/septemberfilms"),
-            InlineKeyboardButton("click this to join", url="t.me/septemberfilms")
+            InlineKeyboardButton ("MYTHRI MOVIES", url="https://t.me/MythriMovies"),
+            InlineKeyboardButton ("MM LINKZ", url="https://t.me/MMlinkz")
             ],[
-            InlineKeyboardButton("click this to join", url="t.me/septemberfilms")
+            InlineKeyboardButton ("MMSUPPORTTGROUP", url="https://t.me/MMsupporttgroup"),
+            InlineKeyboardButton ("MMOFCGROUP", url="https://t.me/MMofcgroup")
+            ],[
+            InlineKeyboardButton ("DEV🧑‍💻", url="https://t.me/iTOMMYSHELBY"),
+            InlineKeyboardButton ("Repository🤖", url="https://github.com/XTHIGHLIGHT")
+            ]]
+            )
+        )
+   
+
+@Highlight.on_message(filters.command("info"))
+async def info_message(bot, messege):
+    text = f"""
+First Name - {message.from_user.first_name}
+Last Name - {message.from_user.last_name}
+UserName - {message.from_user.username}
+Id - {message.from_user.id}
+Mention - {message.from_user.mention}"""
+
+    await message.reply_text(text=text)
+        
+        
+@Highlight.on_message(filters.command("help")) 
+async def start_message(bot, message):
+    await message.reply_photo(
+        photo="https://telegra.ph/file/6f014e3d6c8b90cb6d49e.jpg",
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton ("CLICK HERE", url="https://youtu.be/Af055Eozk9s")
             ]]
             )
         )
 
 
-
-
-HELP_MESSAGE="""
-Hello {} my help means fully detail of coammandshere is
-/id - get your telegram id
-/about - show your full details
-/start - start the bot"""
-
-
-
-
-@Devourd.on_message(filters.command("help")) 
-async def help_message(bot, message):
-
-    await message.reply_text(
-        text=HELP_MESSAGE.format(message.from_user.mention)
-    )
-    
-    
-    
-    
-    
-    
-@Devourd.on_message(filters.command("about")) 
-async def about_message(bot, message):
-    text = f"""
-FIRST NAME - {message.from_user.first_name}
-LAST NAME - {message.from_user.last_name}
-username - @{message.from_user.username}
-id _ -{message.from_user.id}
-mention - {message.from_user.mention}
-source code - githb.com/kamarjahan/pyrorgramfbot"""
-    
-    
-    
-    await message.reply_text(text=text)
-    
-    
-    
-    
-    
-@Devourd.on_message(filters.command("id")) 
-async def id_message(bot, message):
-    text = f"""here is -{message.from_user.id}"""
-    
-    
-    
-    await message.reply_text(text=text)
-    
-
-    
-    
-    
-@Devourd.on_message(filters.command("dev")) 
-async def dev_message(bot, message):
-    text = f"""here is 
-FIRST NAME - DEVOUR
-LAST NAME -  DEVILS
-username -  @DEVOURDEVILS
-id _ -{message.from_user.id}
-mention - @DEVOURDEVILS"""
-    
-    
-    
-    await message.reply_text(text=text)
-    
-    
-    
-    
-    
-    
-    
-
-    
-    
-
-Devourd.run()
+Highlight.run()
